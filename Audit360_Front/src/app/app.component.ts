3efcc5fe56d7052265108053f6f6e6a274168ec0
Leticia Gruñeiro De Mora-Losana <61278393+lgruneirodem@users.noreply.github.com>
2025-06-23 19:57:08 +0200

    Revert "principio de implemnteación multilenguaje"

--- Audit360_Front/src/app/app.component.ts
+++ Audit360_Front/src/app/app.component.ts
@@ -1,42 +1,18 @@
-import { Component, importProvidersFrom } from '@angular/core';
-import { bootstrapApplication } from '@angular/platform-browser';
-import { AppComponent } from '../app/app.component';
+import { Component } from '@angular/core';
 //import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
 import { IonicModule } from '@ionic/angular';
 import { CommonModule } from '@angular/common';
 import {EnvironmentInjector, inject } from '@angular/core';
 import { MenuController } from '@ionic/angular';
 import { PerfilService } from './services/perfil.service';
-import { HttpClientModule,  HttpClient } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 import { addIcons } from 'ionicons';
-import { TranslateService, TranslateModule } from '@ngx-translate/core';
-import { TranslateLoader } from '@ngx-translate/core';
-import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { triangle, ellipse, square } from 'ionicons/icons';
 import { closeOutline, homeOutline, analyticsOutline, settingsOutline,menuOutline,downloadOutline,buildOutline,cardOutline } from 'ionicons/icons';
 
 
 
-export function HttpLoaderFactory(http: HttpClient) {
-  return new TranslateHttpLoader(http);
-}
-
-bootstrapApplication(AppComponent, {
-  providers: [
-    importProvidersFrom(
-      HttpClientModule,
-      TranslateModule.forRoot({
-        defaultLanguage: 'es',
-        loader: {
-          provide: TranslateLoader,
-          useFactory: HttpLoaderFactory,
-          deps: [HttpClient]
-        }
-      })
-    )
-  ]
-}).catch(err => console.error(err));
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
@@ -45,7 +21,7 @@ bootstrapApplication(AppComponent, {
 })
 export class AppComponent {
   usuario: any = {};
-  constructor(private menuCtrl: MenuController, private perfilService: PerfilService, private translate: TranslateService) {
+  constructor(private menuCtrl: MenuController, private perfilService: PerfilService) {
     addIcons({ triangle, ellipse, square,
       'close-outline': closeOutline,
       'home-outline':homeOutline,
@@ -56,18 +32,11 @@ export class AppComponent {
       'build-outline': buildOutline,
       'card-outline' : cardOutline
     });
-
-    translate.addLangs(['es', 'en']);
-    translate.setDefaultLang('es');
-    const browserLang = translate.getBrowserLang();
-    translate.use(browserLang?.match(/es|en/) ? browserLang : 'es');
   }
   ngOnInit() {
     this.perfilService.getUsuario(1).subscribe(data => {
       this.usuario = data;
     });
-
-    
   }
   
 }
